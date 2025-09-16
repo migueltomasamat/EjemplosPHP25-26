31fb8fb2a6574de73129eaab28206e371a675bfb
Miguel Angel <matomas@localhost>
2025-09-16 17:40:59 +0200

    Initial commit

---
+++ ejemplos/tratar-datos.php
@@ -0,0 +1,5 @@
+<?php
+
+    $variable1=$_GET['dividendo'];
+    $variable2=$_GET['divisor'];
+    echo $variable1/$variable2;
---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+       <title>Índice de ejercicios</title>
+</head>
+<body>
+    <h1>Ejercicios de DWES</h1>
+
+
+    <ol>
+        <li><a href="/ejercicios/bucle.php">Creación de un bucle en php</li>
+    </ol>
+<body>
+
+</html>
